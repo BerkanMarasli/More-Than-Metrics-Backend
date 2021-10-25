759d07579754844f3aec8bea437eea39e4f5ebda
Berkan Marasli <berkan.marasli@hotmail.com>
2021-10-25 16:33:18 +0100

    Added endpoints for company and candidate login

--- server.js
+++ server.js
@@ -1,54 +1,74 @@
-const { Pool } = require("pg");
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
-const { v4: uuidv4 } = require("uuid");
+const { Pool } = require("pg")
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const cors = require("cors")
+const { v4: uuidv4 } = require("uuid")
 
-const whitelist = ["http://localhost:3000"];
+const whitelist = ["http://localhost:3000"]
 const corsOptions = {
   credentials: true, // This is important.
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) return callback(null, true);
+    if (whitelist.includes(origin)) return callback(null, true)
 
-    callback(new Error("Not allowed by CORS"));
+    callback(new Error("Not allowed by CORS"))
   },
-};
+}
 
-const app = express();
-app.use(express.json());
-app.use(cors(corsOptions));
+const app = express()
+app.use(express.json())
+app.use(cors(corsOptions))
 
+app.post("/company/login", async (req, res) => {
+  const { company_email, company_password } = req.body
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body
-
-  const client = await usersPool.connect()
-  const getAllData = "SELECT user_id, hashed_password FROM users WHERE email = $1"
+  const client = await moreThanMetricsDB.connect()
+  const getCompanyLoginInfo =
+    "SELECT company_email, company_hashed_password FROM companies WHERE company_email = $1"
   client
-    .query(getAllData, [email])
+    .query(getCompanyLoginInfo, [company_email])
     .then(async queryResult => {
-      const [userInfo] = queryResult.rows
-      if (userInfo.length === 0) {
-        return res.status(400).send("Email is invalid")
+      const [companyInfo] = queryResult.rows
+      if (companyInfo.length === 0) {
+        client.release()
+        return res.status(400).send("Company does not exist")
       }
-      const hashedPass = userInfo.hashed_password
-      const userID = userInfo.user_id
-      const passwordsAreEqual = await bcrypt.compare(password, hashedPass)
-      if (passwordsAreEqual) {
-        const newSessionID = uuidv4()
-        client.query("INSERT INTO sessions (uuid, user_id) VALUES ($1, $2)", [newSessionID, userID])
-        res
-          .cookie("worldBankAppSessionID", newSessionID, {
-            maxAge: 120000,
-          })
-          .status(200)
-          .send("success")
+      const hashedPassword = companyInfo.company_hashed_password
+      const isPasswordValid = await bcrypt.compare(company_password, hashedPassword)
+      if (isPasswordValid) {
+        res.status(200).send("Successfully logged in")
       } else {
         res.status(400).send("Password is invalid")
       }
     })
     .catch(error => {
-      console.log(error)
+      res.status(500).send({ error })
+    })
+  client.release()
+})
+
+app.post("/candidate/login", async (req, res) => {
+  const { candidate_email, candidate_password } = req.body
+
+  const client = await moreThanMetricsDB.connect()
+  const getCandidateLoginInfo =
+    "SELECT candidate_email, candidate_hashed_password FROM candidates WHERE candidate_email = $1"
+  client
+    .query(getCandidateLoginInfo, [candidate_email])
+    .then(async queryResult => {
+      const [candidateInfo] = queryResult.rows
+      if (candidateInfo.length === 0) {
+        client.release()
+        return res.status(400).send("Candidate does not exist")
+      }
+      const hashedPassword = candidateInfo.candidate_hashed_password
+      const isPasswordValid = await bcrypt.compare(candidate_password, hashedPassword)
+      if (isPasswordValid) {
+        res.status(200).send("Successfully logged in")
+      } else {
+        res.status(400).send("Password is invalid")
+      }
+    })
+    .catch(error => {
       res.status(500).send({ error })
     })
   client.release()
