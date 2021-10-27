fae31a0043f18ff3a4b9f335f1bd7c0ef66ce746
Sang Ta <sang_ta_sigma@Sangs-MacBook-Pro.local>
2021-10-27 16:42:53 +0100

    Added in the posting jobs end point, edited the jobs search end point to fit the new requirements, added in a job endpoint for a specific job for their details

--- server-dummyData.js
+++ server-dummyData.js
@@ -13,6 +13,12 @@ INSERT INTO jobs (job_title, job_description, account_id) VALUES ('Coding Coach'
 
 INSERT INTO applications (question_1, answer_1, question_2, answer_2, question_3, answer_3, reviewed, accepted, account_id, job_id) VALUES (1, 'A', 2, 'B', 3, 'C', true, false, 3, 1);
 
+INSERT INTO job_responsibilities (job_id, responsibility) VALUES (1, 'Help people die'),(1, 'Help people survive'),(5, 'teach to sing'),(5, 'teach to dance'),(3, 'use a book'),(4, 'Harry potter spells'),(4, 'wingardium leviosa'),(4, 'wingardium leviosaaaaaaa'),(4, 'wingardium leviosaaaaAAA'),(4, 'wingardium leviosaAAAAAaaaaaAAAAAAaaaAAAaAAaAAAAAaaAaaAAaaA');
 
+INSERT INTO job_technologies (job_id, technology_id) VALUES (1, 1),(1, 4),(5, 5),(5, 7),(3, 11),(4, 10),(4, 3),(4, 6),(4, 2),(4, 1),(5, 10),(8, 3),(9, 6),(3, 2),(3, 1),(6, 10),(7, 3),(8, 6),(7, 2),(3, 1);
+
+INSERT INTO jobs (job_title, job_description, location, salary, account_id) VALUES ('Coding Coach', 'Coach people coding', 'London', 'Competitive LOL', 17), ('CEO', 'Rich', 'London', '£££', 18), ('Software Designer', 'Design Software', 'Manchester', '£48000', 17), ('Janitor', 'Clean stuff', 'Chelsea', '£32000', 18), ('MEGALORD', 'Be Evil', 'Dublin', '12 Souls p/h', 19), ('Candy Man', 'Sell Candy', 'Oxford', '£28000', 19), ('Candy Boy', 'Buy Candy', 'Oxford', '-£1000', 19), ('Developer', 'develop things', 'London', '£30000', 20), ('Developer', 'develop things', 'London', '£30000', 20), ('Coding Coach', 'Coach coding', 'London', '£40000', 20), ('Machine', 'Learning', 'Office', '£2', 19), ('Flower', 'Grow', 'Garden', '10 drops of sunlight', 18);
+
+INSERT INTO companies(company_name, company_bio, location, company_number_of_employees_id,company_female_employee_percentage, company_retention_rate, account_id) VALUES ('Bigma Labs', 'Sigma labs but bigger', 'Sillicon Valley', 4, 12,23,17), ('Ligma Sabs', 'What's Ligma?', 'Cherry Well', 5, 10, 27, 18), ('Heidi''s Hut', 'Push her off a cliff', 'Somewhere in the countryside', 7, 54,9,19), ('Happy Harvester', 'Ruining lives by the second', 'Texas', 2, 3,54,20)
 
 */
--- server-schema.js
+++ server-schema.js
@@ -2,105 +2,123 @@
 DROP TABLE account_type CASCADE; DROP TABLE years_in_industry CASCADE; DROP TABLE number_of_employees CASCADE; DROP TABLE technologies CASCADE; DROP TABLE accounts CASCADE; DROP TABLE candidates CASCADE; DROP TABLE companies CASCADE; DROP TABLE candidates_technologies CASCADE;
 */
 
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 
 const DBSTRING =
-  "postgres://hjtqvwqx:i-lgggJgY-howhBMFWrhsLpMOel53sxn@surus.db.elephantsql.com/hjtqvwqx"
+  "postgres://hjtqvwqx:i-lgggJgY-howhBMFWrhsLpMOel53sxn@surus.db.elephantsql.com/hjtqvwqx";
 
-const moreThanMetricsDB = new Pool({ connectionString: DBSTRING })
+const moreThanMetricsDB = new Pool({ connectionString: DBSTRING });
 
 async function createTables() {
-  const client = await moreThanMetricsDB.connect()
+  const client = await moreThanMetricsDB.connect();
 
   // Account Type Table
   const createAccountTypeTable =
-    "CREATE TABLE account_type (account_type_id SERIAL PRIMARY KEY, account_type_category TEXT NOT NULL);"
+    "CREATE TABLE account_type (account_type_id SERIAL PRIMARY KEY, account_type_category TEXT NOT NULL);";
   client
     .query(createAccountTypeTable)
     .then(() => console.log("Created account_type table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Years In Industry Table
   const createYearsInIndustryTable =
-    "CREATE TABLE years_in_industry (years_in_industry_id SERIAL PRIMARY KEY, category TEXT NOT NULL);"
+    "CREATE TABLE years_in_industry (years_in_industry_id SERIAL PRIMARY KEY, category TEXT NOT NULL);";
   client
     .query(createYearsInIndustryTable)
     .then(() => console.log("Created years_in_industry table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Number Of Employees Table
   const createNumberOfEmployeesTable =
-    "CREATE TABLE number_of_employees (number_of_employees_id SERIAL PRIMARY KEY, category TEXT NOT NULL);"
+    "CREATE TABLE number_of_employees (number_of_employees_id SERIAL PRIMARY KEY, category TEXT NOT NULL);";
   client
     .query(createNumberOfEmployeesTable)
     .then(() => console.log("Created number_of_employees table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Technologies Table
   const createTechnologiesTable =
-    "CREATE TABLE technologies (technology_id SERIAL PRIMARY KEY, technology_name TEXT NOT NULL);"
+    "CREATE TABLE technologies (technology_id SERIAL PRIMARY KEY, technology_name TEXT NOT NULL);";
   client
     .query(createTechnologiesTable)
     .then(() => console.log("Created technologies table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Prompts Table
   const createPromptTable =
-    "CREATE TABLE prompts (prompt_id SERIAL PRIMARY KEY, prompt TEXT NOT NULL);"
+    "CREATE TABLE prompts (prompt_id SERIAL PRIMARY KEY, prompt TEXT NOT NULL);";
   client
     .query(createPromptTable)
     .then(() => console.log("Created prompt table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Accounts Table
   const createAccountsTable =
-    "CREATE TABLE accounts (account_id SERIAL PRIMARY KEY, account_email TEXT NOT NULL, account_hashed_password TEXT NOT NULL, account_type_id INTEGER NOT NULL, FOREIGN KEY (account_type_id) REFERENCES account_type(account_type_id));"
+    "CREATE TABLE accounts (account_id SERIAL PRIMARY KEY, account_email TEXT NOT NULL, account_hashed_password TEXT NOT NULL, account_type_id INTEGER NOT NULL, FOREIGN KEY (account_type_id) REFERENCES account_type(account_type_id));";
   client
     .query(createAccountsTable)
     .then(() => console.log("Created accounts table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Candidates Table
   const createCandidatesTable =
-    "CREATE TABLE candidates (candidate_id SERIAL PRIMARY KEY, candidate_name TEXT NOT NULL, candidate_phone_number BIGINT NOT NULL, candidate_years_in_industry_id INTEGER NOT NULL, account_id INTEGER NOT NULL, FOREIGN KEY (candidate_years_in_industry_id) REFERENCES years_in_industry(years_in_industry_id), FOREIGN KEY (account_id) REFERENCES accounts(account_id));"
+    "CREATE TABLE candidates (candidate_id SERIAL PRIMARY KEY, candidate_name TEXT NOT NULL, candidate_phone_number BIGINT NOT NULL, candidate_years_in_industry_id INTEGER NOT NULL, account_id INTEGER NOT NULL, FOREIGN KEY (candidate_years_in_industry_id) REFERENCES years_in_industry(years_in_industry_id), FOREIGN KEY (account_id) REFERENCES accounts(account_id));";
   client
     .query(createCandidatesTable)
     .then(() => console.log("Created candidates table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Companies Table
   const createCompaniesTable =
-    "CREATE TABLE companies (company_id SERIAL PRIMARY KEY, company_name TEXT NOT NULL, company_bio TEXT NOT NULL, company_number_of_employees_id INTEGER NOT NULL, company_female_employee_percentage INTEGER NOT NULL, company_retention_rate INTEGER NOT NULL, account_id INTEGER NOT NULL, FOREIGN KEY (company_number_of_employees_id) REFERENCES number_of_employees(number_of_employees_id), FOREIGN KEY (account_id) REFERENCES accounts(account_id));"
+    "CREATE TABLE companies (company_id SERIAL PRIMARY KEY, company_name TEXT NOT NULL, company_bio TEXT NOT NULL, location TEXT NOT NULL, company_number_of_employees_id INTEGER NOT NULL, company_female_employee_percentage INTEGER NOT NULL, company_retention_rate INTEGER NOT NULL, account_id INTEGER NOT NULL, FOREIGN KEY (company_number_of_employees_id) REFERENCES number_of_employees(number_of_employees_id), FOREIGN KEY (account_id) REFERENCES accounts(account_id));";
   client
     .query(createCompaniesTable)
     .then(() => console.log("Created companies table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
   // Candidates Technologies Table
   const createCandidatesTechnologiesTable =
-    "CREATE TABLE candidates_technologies (account_id INTEGER NOT NULL, technology_id INTEGER NOT NULL, FOREIGN KEY (account_id) REFERENCES accounts(account_id), FOREIGN KEY (technology_id) REFERENCES technologies(technology_id));"
+    "CREATE TABLE candidates_technologies (account_id INTEGER NOT NULL, technology_id INTEGER NOT NULL, FOREIGN KEY (account_id) REFERENCES accounts(account_id), FOREIGN KEY (technology_id) REFERENCES technologies(technology_id));";
   client
     .query(createCandidatesTechnologiesTable)
-    .then(() => console.log("Created candidates_technologies table successfully"))
-    .catch(error => console.log(error))
+    .then(() =>
+      console.log("Created candidates_technologies table successfully")
+    )
+    .catch((error) => console.log(error));
 
   // Jobs Table
   const createJobsTable =
-    "CREATE TABLE jobs (job_id SERIAL PRIMARY KEY, job_title TEXT NOT NULL, job_description TEXT NOT NULL, account_id INTEGER NOT NULL, FOREIGN KEY (account_id) REFERENCES accounts(account_id));"
+    "CREATE TABLE jobs (job_id SERIAL PRIMARY KEY, job_title TEXT NOT NULL, job_description TEXT NOT NULL, location TEXT NOT NULL, salary TEXT NOT NULL, account_id INTEGER NOT NULL, FOREIGN KEY (account_id) REFERENCES accounts(account_id));";
   client
     .query(createJobsTable)
     .then(() => console.log("Created jobs table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
+
+  //Job responsibilities table
+  const createJobsResponsibilitiesTable =
+    "CREATE TABLE job_responsibilities (job_id INTEGER NOT NULL, responsibility TEXT NOT NULL, FOREIGN KEY (job_id) REFERENCES jobs(job_id))";
+  client
+    .query(createJobsResponsibilitiesTable)
+    .then(() => console.log("Created jobs responsibilities table successfully"))
+    .catch((error) => console.log(error));
+
+  //Job technologies table
+  const createJobsTechnologiesTable =
+    "CREATE TABLE job_technologies (job_id INTEGER NOT NULL, technology_id INTEGER NOT NULL, FOREIGN KEY (job_id) REFERENCES jobs(job_id), FOREIGN KEY (technology_id) REFERENCES technologies(technology_id))";
+  client
+    .query(createJobsTechnologiesTable)
+    .then(() => console.log("Created jobs technologies table successfully"))
+    .catch((error) => console.log(error));
 
   // Applications Table
   const createApplicationsTable =
-    "CREATE TABLE applications (application_id SERIAL PRIMARY KEY, question_1 INTEGER NOT NULL, answer_1 TEXT NOT NULL, question_2 INTEGER NOT NULL, answer_2 TEXT NOT NULL, question_3 INTEGER NOT NULL, answer_3 TEXT NOT NULL, reviewed BOOL NOT NULL, accepted BOOL NOT NULL, account_id INTEGER NOT NULL, job_id INTEGER NOT NULL, FOREIGN KEY (account_id) REFERENCES accounts(account_id), FOREIGN KEY (job_id) REFERENCES jobs(job_id), FOREIGN KEY (question_1) REFERENCES prompts(prompt_id), FOREIGN KEY (question_2) REFERENCES prompts(prompt_id), FOREIGN KEY (question_3) REFERENCES prompts(prompt_id));"
+    "CREATE TABLE applications (application_id SERIAL PRIMARY KEY, question_1 INTEGER NOT NULL, answer_1 TEXT NOT NULL, question_2 INTEGER NOT NULL, answer_2 TEXT NOT NULL, question_3 INTEGER NOT NULL, answer_3 TEXT NOT NULL, reviewed BOOL NOT NULL, accepted BOOL NOT NULL, account_id INTEGER NOT NULL, job_id INTEGER NOT NULL, FOREIGN KEY (account_id) REFERENCES accounts(account_id), FOREIGN KEY (job_id) REFERENCES jobs(job_id), FOREIGN KEY (question_1) REFERENCES prompts(prompt_id), FOREIGN KEY (question_2) REFERENCES prompts(prompt_id), FOREIGN KEY (question_3) REFERENCES prompts(prompt_id));";
   client
     .query(createApplicationsTable)
     .then(() => console.log("Created applications table successfully"))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 
-  client.release()
+  client.release();
 }
 
-createTables()
+createTables();
