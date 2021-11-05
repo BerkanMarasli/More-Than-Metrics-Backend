a96402518823c25a0c0b5d175664f759bfd9594c
Berkan Marasli <berkan.marasli@hotmail.com>
2021-11-05 12:28:18 +0000

    Added domain to cookies

--- server-logic/account-logic.js
+++ server-logic/account-logic.js
@@ -242,11 +242,13 @@ exports.loginUser = async function loginUser(req, res, moreThanMetricsDB) {
                         maxAge: 86000000,
                         sameSite: "none",
                         secure: true,
+                        domain: "morethanmetrics.netlify.app",
                     })
                     .cookie("moreThanMetricsID", userID, {
                         maxAge: 86000000,
                         sameSite: "none",
                         secure: true,
+                        domain: "morethanmetrics.netlify.app",
                     })
                     .send({ message: "Successfully logged in!", type: accountInfo.account_type_category, userID: userID, url: url })
             } else {
