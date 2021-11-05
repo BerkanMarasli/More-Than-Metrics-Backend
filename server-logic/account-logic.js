9996a06bbf2bd5e0ba50a929104e4e562c7c4bff
Berkan Marasli <berkan.marasli@hotmail.com>
2021-11-05 12:15:45 +0000

    Added sameSite and secure attributes to cookies

--- server-logic/account-logic.js
+++ server-logic/account-logic.js
@@ -240,9 +240,13 @@ exports.loginUser = async function loginUser(req, res, moreThanMetricsDB) {
                 res.status(200)
                     .cookie("moreThanMetricsAT", accountInfo.account_type_category, {
                         maxAge: 86000000,
+                        sameSite: "none",
+                        secure: true,
                     })
                     .cookie("moreThanMetricsID", userID, {
                         maxAge: 86000000,
+                        sameSite: "none",
+                        secure: true,
                     })
                     .send({ message: "Successfully logged in!", type: accountInfo.account_type_category, userID: userID, url: url })
             } else {
